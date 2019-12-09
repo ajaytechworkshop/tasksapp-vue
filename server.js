87416bcf619256323392d91ddf5e2ae04f785c24
Ajay <ajaytechworkshop@gmail.com>
2019-12-09 14:35:17 +0100

    Fixed server.js to run the app as node app

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const serveStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
-app.use('/', serveStatic(path.join('__dirname/dist')));
+app.use(serveStatic(`${__dirname}/dist`));
 
 // Serve the app in the below port
 const port = process.env.PORT || 7000;
-app.listen(port);
-console.log('Tasks webapp  started..', +port);
+app.listen(port, () => {
+  console.log('Tasks webapp is up and running..', +port);
+});
