1f89da7b556fa6bca9b4dc3a7080ee975f54434b
Ajay <ajaytechworkshop@gmail.com>
2019-12-09 23:59:24 +0100

    Update and Remove completed

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -4,6 +4,7 @@
       <div class="col-sm-10">
         <h1>Tasks</h1>
         <hr><br><br>
+        <alert :message="message" v-if="showMessage"></alert>
         <button type="button" class="btn btn-success btn-sm" v-b-modal.task-modal>New Tasks</button>
         <br><br>
         <table class="table table-hover">
@@ -12,7 +13,8 @@
               <!--th scope="col">Date</th-->
               <th scope="col">Task</th>
               <th scope="col">Description</th>
-              <th scope="col">Done?</th>
+              <th scope="col">Done</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -26,8 +28,11 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Remove</button>
+                  <button type="button"
+                  class="btn btn-warning btn-sm"
+                  v-b-modal.task-update-modal @click="editTaskfn(task)">Edit</button>
+                  <button type="button" class="btn btn-danger btn-sm"
+                   @click="onDeleteTask(task)">Remove</button>
                 </div>
               </td>
             </tr>
@@ -60,14 +65,51 @@
         <b-button type="reset" variant="danger">Reset</b-button>
       </b-form>
     </b-modal>
+    <b-modal ref="editTaskModal"
+         id="task-update-modal"
+         title="Update"
+         hide-footer>
+  <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+  <b-form-group id="form-name-edit-group"
+                label="Name:"
+                label-for="form-name-edit-input">
+      <b-form-input id="form-name-edit-input"
+                    type="text"
+                    v-model="editTask.name"
+                    required
+                    placeholder="Task Name">
+      </b-form-input>
+    </b-form-group>
+    <b-form-group id="form-description-edit-group"
+                  label="Description:"
+                  label-for="form-description-edit-input">
+        <b-form-input id="form-description-edit-input"
+                      type="text"
+                      v-model="editTask.description"
+                      required
+                      placeholder="Task Description">
+        </b-form-input>
+      </b-form-group>
+    <b-form-group id="form-done-edit-group">
+      <b-form-checkbox-group v-model="editTask.done" id="form-checks">
+        <b-form-checkbox value="true">Done?</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+    <b-button-group>
+      <b-button type="submit" variant="primary">Update</b-button>
+      <b-button type="reset" variant="danger">Cancel</b-button>
+    </b-button-group>
+  </b-form>
+</b-modal>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Alert from './Alert.vue';
 
-const host = 'http://localhost:5000';
-// const host = 'https://tasksnodeapp.herokuapp.com';
+// const host = 'http://localhost:4000';
+const host = 'https://tasksnodeapp.herokuapp.com';
 
 export default {
   data() {
@@ -78,11 +120,21 @@ export default {
         description: '',
         done: [],
       },
+      editTask: {
+        name: '',
+        description: '',
+        done: [],
+      },
+      message: '',
+      showMessage: false,
     };
+  },
+  components: {
+    alert: Alert,
   },
   methods: {
     getTasks() {
-      const path = 'https://tasksnodeapp.herokuapp.com/tasks';
+      const path = `${host}/tasks`;
       axios.get(path).then((res) => {
         this.tasks = res.data;
         console.log(res.data.tasks);
@@ -92,17 +144,22 @@ export default {
     },
     addTaskfn(payload) {
       const path = `${host}/tasks`;
-      axios.post(path, payload).then(() => {
-        this.getTasks();
-      }).catch((error) => {
-        console.error(error);
-        this.getTasks();
-      });
+      axios.post(path, payload)
+        .then(() => {
+          this.getTasks();
+          this.message = 'New Task added';
+          this.showMessage = true;
+        }).catch(() => {
+          this.getTasks();
+        });
     },
     initForm() {
       this.addTask.name = '';
       this.addTask.description = '';
       this.addTask.done = [];
+      this.editTask.name = '';
+      this.editTask.description = '';
+      this.editTask.done = [];
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -121,6 +178,52 @@ export default {
       evt.preventDefault();
       this.$refs.addTaskModal.hide();
       this.initForm();
+    },
+    editTaskfn(task) {
+      this.editTask = task;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editTaskModal.hide();
+      let done = false;
+      if (this.editTask.done[0]) done = true;
+      const payload = {
+        name: this.editTask.name,
+        description: this.editTask.description,
+        done,
+      };
+      this.updateTask(payload, this.editTask._id);
+    },
+    updateTask(payload, taskId) {
+      const path = `${host}/tasks/${taskId}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getTasks();
+          this.message = 'Task updated';
+          this.showMessage = true;
+        }).catch(() => {
+          this.getTasks();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editTaskModal.hide();
+      this.initForm();
+      this.getTasks();
+    },
+    onDeleteTask(task) {
+      /* eslint no-underscore-dangle: 0 */
+      this.removeTask(task._id);
+    },
+    removeTask(taskId) {
+      const path = `${host}/tasks/${taskId}`;
+      axios.delete(path).then(() => {
+        this.getTasks();
+        this.message = 'Task removed';
+        this.showMessage = true;
+      }).catch(() => {
+        this.getTasks();
+      });
     },
   },
   created() {
